c3e9709f1f5c9addd9780c32cbe33acaf8274c16
Beca Parker <beca.parker@red-gate.com>
2022-12-14 09:26:31 +0000

    WIP check if tree visible in all directions

--- Day8/TreeCheckerTests.cs
+++ Day8/TreeCheckerTests.cs
@@ -57,7 +57,7 @@ public class TreeCheckerTests
 
 }
 
-public class TreeChecker
+public static class TreeChecker
 {
     public static int CountOfVisible(string grid)
     {
@@ -67,7 +67,46 @@ public class TreeChecker
 
     public static int CountOfInteriorTallTrees(string grid)
     {
-        return 5;
+        var treeRows = grid.Split(Environment.NewLine);
+        var treeColumns = new List<string>();
+        for (int i = 0; i < treeRows[0].Count(); i++)
+        {
+            treeColumns.Add(new string(treeRows.Select(x => x[i]).ToArray()));
+        }
+        
+        var visibleTrees = 0;
+
+        for (int i = 1; i < treeRows[0].Count() -1; i++)
+        {
+            var row = treeRows[i];
+            for (int j = 1; j < row.Count() -1; j++)
+            {
+                var tree = row[j];
+                //Look East and West
+                if (tree >= row.Remove(j).Max())
+                {
+                    visibleTrees++;
+                }
+            }
+        }
+        
+        for (int k = 1; k < treeColumns[0].Count() -1; k++)
+        {
+            var column = treeColumns[k];
+            for (int l = 1; l < column.Count() -1; l++)
+            {
+                var tree = column[l];
+                //Look North and South
+                if (tree >= column.Remove(l).Max())
+                {
+                    visibleTrees++;
+                }
+            }
+        }
+        
+        // TODO: Are we incrementing visibleTrees twice if a tree is visible in more than one direction? Use or logic
+        
+        return visibleTrees;
     }
 
     public static int CountOfPerimeterTrees(string grid)
